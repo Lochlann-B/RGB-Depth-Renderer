68ff019b236333a712de5d8fda4f619831185508
Lochlann-B <locky_baker@hotmail.co.uk>
2024-01-09 21:38:20 +0000

    OpenGL Boilerplate

---
+++ RGBDReconstruction.Application/Application.cs
@@ -0,0 +1,105 @@
+﻿using System.ComponentModel;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace RGBDReconstruction.Application;
+
+public class Application(int width, int height, string title) : GameWindow(GameWindowSettings.Default,
+    new NativeWindowSettings() { ClientSize = (width, height), Title = title, Flags = ContextFlags.ForwardCompatible })
+{
+    
+    // TODO: Get rid of this tutorial stuff!
+    private readonly float[] _vertices = {
+        0.5f,  0.5f, 0.0f,  // top right
+        0.5f, -0.5f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f,  // bottom left
+        -0.5f,  0.5f, 0.0f   // top left
+    };
+
+    private readonly uint[] _indices =
+    {
+        0, 1, 3,
+        1, 2, 3
+    };
+
+    private int _vertexBufferObject;
+    private int _elementBufferObject;
+    private int _vertexArrayObject;
+
+    private Shader _shader;
+
+    protected override void OnLoad()
+    {
+        // Runs once when the window opens. Put initialization code here!
+        base.OnLoad();
+
+        GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+        
+        // You can bind several buffers at once as long as they are of different types.
+        // VBO is an array buffer type object
+        _vertexBufferObject = GL.GenBuffer();
+        
+        GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
+        // ^ Now any calls to arraybuffer target will actually be referring to the VBO
+        
+        // BufferData is what lets us put user-defined data into a buffer
+        // Static - likely won't change that much
+        // Dynamic - likely to change a lot
+        // Stream - Will change every time it is drawn
+        GL.BufferData(BufferTarget.ArrayBuffer, _vertices.Length * sizeof(float), _vertices, BufferUsageHint.StaticDraw);
+        
+        // VAO - Vertex Array Object - specifies how the vertex attribute information is stored, formatted, and which buffers the data comes from
+        _vertexArrayObject = GL.GenVertexArray();
+        GL.BindVertexArray(_vertexArrayObject);
+        
+        _elementBufferObject = GL.GenBuffer();
+        GL.BindBuffer(BufferTarget.ElementArrayBuffer, _elementBufferObject);
+        GL.BufferData(BufferTarget.ElementArrayBuffer, _indices.Length * sizeof(uint), _indices, BufferUsageHint.StaticDraw);
+        
+        // The 'index' param below refers to the location in the shader program we are putting data into.
+        // That is, the layout (location = 0) in shader.vert
+        GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+        GL.EnableVertexAttribArray(0);
+        
+        // The VBO that the VAO takes its data from is determined by the VBO currently bound to the ArrayBuffer (i.e. that on line 43)
+
+        _shader = new Shader("./shader.vert", "./shader.frag");
+        _shader.Use();
+    }
+
+    protected override void OnRenderFrame(FrameEventArgs args)
+    {
+        base.OnRenderFrame(args);
+        
+        GL.Clear(ClearBufferMask.ColorBufferBit);
+
+        // Render frame code goes here - run every frame
+        _shader.Use();
+        GL.BindVertexArray(_vertexArrayObject);
+        GL.DrawElements(PrimitiveType.Triangles, _indices.Length, DrawElementsType.UnsignedInt, 0);
+        
+        SwapBuffers();
+    }
+
+    protected override void OnResize(ResizeEventArgs e)
+    {
+        base.OnResize(e);
+        
+        GL.Viewport(0, 0, e.Width, e.Height);
+    }
+
+    protected override void OnUpdateFrame(FrameEventArgs args)
+    {
+        base.OnUpdateFrame(args);
+
+        if (KeyboardState.IsKeyDown(Keys.Escape))
+        {
+            Close();
+        }
+    }
+    
+    
+}
---
+++ RGBDReconstruction.Application/Shader.cs
@@ -0,0 +1,101 @@
+﻿using OpenTK.Graphics.OpenGL4;
+
+namespace RGBDReconstruction.Application;
+
+public class Shader
+{
+    private int Handle;
+
+    public Shader(string vertexPath, string fragmentPath)
+    {
+        int VertexShader;
+        int FragmentShader;
+        
+        string VertexShaderSource = File.ReadAllText(vertexPath);
+        string FragmentShaderSource = File.ReadAllText(fragmentPath);
+
+        VertexShader = GL.CreateShader(ShaderType.VertexShader);
+        GL.ShaderSource(VertexShader, VertexShaderSource);
+
+        FragmentShader = GL.CreateShader(ShaderType.FragmentShader);
+        GL.ShaderSource(FragmentShader, FragmentShaderSource);
+        
+        GL.CompileShader(VertexShader);
+        
+        GL.GetShader(VertexShader, ShaderParameter.CompileStatus, out int successS);
+        if (successS == 0)
+        {
+            string infoLog = GL.GetShaderInfoLog(VertexShader);
+            // TODO: Change to error style reporting
+            Console.WriteLine(infoLog);
+        }
+        
+        GL.CompileShader(FragmentShader);
+        
+        GL.GetShader(FragmentShader, ShaderParameter.CompileStatus, out int successF);
+        if (successF == 0)
+        {
+            string infoLog = GL.GetShaderInfoLog(FragmentShader);
+            // TODO: Change to error style reporting
+            Console.WriteLine(infoLog);
+        }
+
+        Handle = GL.CreateProgram();
+        
+        GL.AttachShader(Handle, VertexShader);
+        GL.AttachShader(Handle, FragmentShader);
+        
+        GL.LinkProgram(Handle);
+        
+        GL.GetProgram(Handle, GetProgramParameterName.LinkStatus, out int successLink);
+        if (successLink == 0)
+        {
+            string infoLog = GL.GetProgramInfoLog(Handle);
+            Console.WriteLine(infoLog);
+        }
+        
+        GL.DetachShader(Handle, VertexShader);
+        GL.DetachShader(Handle, FragmentShader);
+        GL.DeleteShader(FragmentShader);
+        GL.DeleteShader(VertexShader);
+    }
+
+    public void Use()
+    {
+        GL.UseProgram(Handle);
+    }
+    
+    public int GetAttribLocation(string attribName)
+    {
+        return GL.GetAttribLocation(Handle, attribName);
+    }
+    
+    private bool _disposedValue = false;
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!_disposedValue)
+        {
+            GL.DeleteProgram(Handle);
+
+            _disposedValue = true;
+        }
+    }
+    
+
+    ~Shader()
+    {
+        if (_disposedValue == false)
+        {
+            // TODO: Change to error warning
+            Console.WriteLine("GPU Resource leak! Did you forget to call Dispose()?");
+        }
+    }
+
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+}
